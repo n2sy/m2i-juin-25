feb739ca5dab565d33b5fa561740d10c2c77ad4a
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-06-03 16:32:25 +0100

    toutes les notions des TDFs

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,7 +10,18 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  defaultLanguage = 'php';
+  monCommentiare = 'Rien à signaler...';
   submitHandler(f: NgForm) {
     console.log(f);
+  }
+
+  resetHandler(f: NgForm) {
+    f.reset();
+  }
+
+  genererInfos(f: NgForm) {
+    // f.form.patchValue({ email: 'nidhal@gmail.com', password: '123456' });
+    f.form.patchValue({ email: '', password: '' });
   }
 }
