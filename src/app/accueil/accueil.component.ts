a35529b0b188b59e8a4015143094f49f068da623
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-06-04 10:26:58 +0100

    injection de httpClient

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { ExpObsComponent } from '../exp-obs/exp-obs.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-accueil',
@@ -11,6 +12,22 @@ import { ExpObsComponent } from '../exp-obs/exp-obs.component';
 })
 export class AccueilComponent {
   private router = inject(Router);
+  private http = inject(HttpClient);
+
+  ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/useeers').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('Au revoir');
+      },
+    });
+  }
+
   goToCv() {
     this.router.navigateByUrl('cv');
   }
